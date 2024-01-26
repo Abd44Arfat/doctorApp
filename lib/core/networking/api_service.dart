c86fc0bd5ae983f65251bea2e30d7c738ed737ae
Abdelrahman arfat <abdoarfat924@gmail.com>
2024-01-26 10:15:17 +0200

    added functionality signup add regex for pohone number integreate with sign up end point

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:doctor/core/networking/api_constants.dart';
 import 'package:doctor/features/login/data/models/login_request_body.dart';
 import 'package:doctor/features/login/data/models/login_response.dart';
+import 'package:doctor/features/sign_up/data/models/sign_up_request_body.dart';
+import 'package:doctor/features/sign_up/data/models/sign_up_response.dart';
 
 import 'package:retrofit/retrofit.dart';
 
@@ -14,5 +16,9 @@ abstract class ApiService {
   @POST(ApiConstants.login)
   Future<LoginResponse> login(
     @Body() LoginRequestBody loginRequestBody,
+  );
+    @POST(ApiConstants.signup)
+  Future<SignupResponse> signup(
+    @Body() SignupRequestBody signupRequestBody,
   );
 }
